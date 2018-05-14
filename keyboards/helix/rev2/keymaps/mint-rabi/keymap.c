15a3917f1e6abf135f03405554fdd47fa6d9bbee
tetsuro yamaguchi <tetsuro.yamaguchi@d2c.co.jp>
2018-05-14 13:53:28 +0900

    ちょいちょい修正

--- keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
+++ keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
@@ -72,15 +72,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |¥_ / S|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Grv/AJ| Esc  | Alt  | GUI  | LNG1 |Sp/Low|Bksp  |Enter |Raise | LNG2 | Left | Down |  Up  |Right |
+   * |Grv/AJ| Esc  | Alt  | GUI  | Space|Lower |Bksp  |Enter |Raise | LNG2 | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
-      KC_1,     KC_2,    KC_3,     KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JA_HAT, \
-      KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
-      KC_LCTL,  KC_A,    KC_S,     KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON), \
-      KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
-      KC_GRV,   KC_ESC,  KC_LALT,  KC_LGUI, KC_SPC  ,LOWER,  KC_BSPC, KC_ENT,  RAISE,   KC_LANG2,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_1,     KC_2,    KC_3,     KC_4,    KC_5,   KC_6,                     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JA_HAT, \
+      KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
+      KC_LCTL,  KC_A,    KC_S,     KC_D,    KC_F,   KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON), \
+      KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
+      KC_GRV,   KC_ESC,  KC_LALT,  KC_LGUI, LOWER,  KC_SPC, KC_BSPC, KC_ENT,  RAISE,   KC_RALT ,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
