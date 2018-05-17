bb557fa8af098d411e5bb75d9e835e00e588a73a
Tetsurou Yamagchi <mint.rabi@gmail.com>
2018-05-18 08:59:33 +0900

    主に親指周り修正

--- keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
+++ keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
@@ -70,16 +70,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  | :* /C|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |   BS |  DEL |   N  |   M  |   ,  |   .  |   /  |¥_ / S|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |  ESC |  DEL |   N  |   M  |   ,  |   .  |   /  |¥_ / S|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Grv   | Esc  | Alt  | GUI  | Alt  |Lower |Space |Enter |Dl/RI | LNG2 | Left | Down |  Up  |Right |
+   * |Grv   | Esc  | Alt  | GUI  | Alt  |Lower |Space |Enter |Raise | LNG2 | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
       KC_1,     KC_2,    KC_3,     KC_4,    KC_5,   KC_6,                    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JA_HAT, \
       KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
       KC_LCTL,  KC_A,    KC_S,     KC_D,    KC_F,   KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON), \
-      KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,  KC_BSPC, KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
+      KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,  KC_ESC , KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
       KC_GRV,   KC_ESC,  KC_LALT,  KC_LGUI, KC_LALT,LOWER, KC_SPC , KC_ENT,  RAISE,   KC_RALT ,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |      |      | Home | End  |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      |      |  DEL |      |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = KEYMAP( \
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_END,  _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, _______, _______, _______, _______, _______, _______, KC_DEL , _______, _______, _______, _______, _______, _______ \
       ),
 
   /* Raise
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |      |      |PageDn|PageUp|      |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      | BKSP |      |      |      | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = KEYMAP( \
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, _______, _______, _______, _______, _______, KC_BSPC, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Adjust (Lower + Raise)
