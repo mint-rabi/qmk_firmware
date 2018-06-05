c80e933da124174cd44f578ba67471673dfba68c
tetsuro yamaguchi <tetsuro.yamaguchi@d2c.co.jp>
2018-06-05 11:22:41 +0900

    トラックボールのスクロール制御用に無変換を追加

--- keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
+++ keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |  ESC |  DEL |   N  |   M  |   ,  |   .  |   /  |¥_ / S|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Grv   | Esc  | Alt  | GUI  | Alt  |Lower |Space |Enter |Raise | LNG2 | Left | Down |  Up  |Right |
+   * |Grv   | Esc  | MuHen| GUI  | Alt  |Lower |Space |Enter |Raise | LNG2 | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
       KC_LCTL,  KC_A,    KC_S,     KC_D,    KC_F,   KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON), \
       KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,  KC_ESC , KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
-      KC_GRV,   KC_ESC,  KC_LALT,  KC_LGUI, KC_LALT,LOWER, KC_SPC , KC_ENT,  RAISE,   KC_RALT ,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_GRV,   KC_ESC,  KC_MHEN,  KC_LGUI, KC_LALT,LOWER, KC_SPC , KC_ENT,  RAISE,   KC_RALT ,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
