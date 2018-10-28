f3fad6afe24c9980d5b6a6b3e77830679b91668a
mint-rabi <mint.rabi@gmail.com>
2018-10-28 21:48:02 +0900

    fortitude60å´ã«Helixãåããã

--- keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
+++ keyboards/helix/rev2/keymaps/mint-rabi/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
       KC_LCTL,  KC_A,    KC_S,     KC_D,    KC_F,   KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON), \
       KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,  JA_LBRC, JA_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN) , \
-      KC_UP,    KC_DOWN, KC_LGUI,  KC_LALT, LOWER  ,KC_SPC,KC_BSPC, KC_ESC,  KC_ENT,  RAISE   ,KC_RALT, KC_GRV , KC_LEFT, KC_RGHT \
+      XXXXXXX,  XXXXXXX, KC_LGUI,  KC_LALT, LOWER  ,KC_SPC,KC_BSPC, KC_ESC,  KC_ENT,  RAISE   ,KC_RALT, KC_GRV , XXXXXXX, XXXXXXX \
       ),
 
   /* Lower
@@ -121,8 +121,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+      KC_GRV,  KC_1,    KC_UP,   KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
