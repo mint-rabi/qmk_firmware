4fec68e60f2027dd59b6cea4689bc718cac2d754
mint-rabi <mint.rabi@gmail.com>
2019-04-16 13:41:15 +0900

    idobo fix

--- keyboards/idobo/keymaps/mint-rabi/keymap.c
+++ keyboards/idobo/keymaps/mint-rabi/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | LEFT   | UP     | RIGHT  | N      | M      | ,      | .      | /      | ¥_ / S |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * |        |        | LGUI   | LALT   | FN     | SPACE  | BACKSP | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |
+ * |        |        | LGUI   | LALT   | FN     | SPACE  | BACKSP | DOWN   | ESC    | Enter  | FN     | RALT   | GRV    |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_HOME, KC_BSLS, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_END , KC_DEL,  KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON),  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LEFT, KC_UP,   KC_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN), \
-    XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT, MO(_FN),  KC_SPC,  KC_BSPC, KC_DOWN, KC_ESC,  KC_SPC,  MO(_FN), KC_RALT, KC_GRV,  XXXXXXX, XXXXXXX  \
+    XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT, MO(_FN),  KC_SPC,  KC_BSPC, KC_DOWN, KC_ESC,  KC_ENT,  MO(_FN), KC_RALT, KC_GRV,  XXXXXXX, XXXXXXX  \
  ),
 
 /* FUNCTION
