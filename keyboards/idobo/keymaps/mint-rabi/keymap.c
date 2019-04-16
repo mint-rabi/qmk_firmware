acacc91195a5071279c49a33f26b4209e10ae4dc
mint-rabi <mint.rabi@gmail.com>
2019-04-16 13:57:27 +0900

    idobo fix

--- keyboards/idobo/keymaps/mint-rabi/keymap.c
+++ keyboards/idobo/keymaps/mint-rabi/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ^      | 1      | 2      | 3      | 4      | 5      | [      | `      | ]      | 6      | 7      | 8      | 9      | 0      |  -     |
+ * | ^      | 1      | 2      | 3      | 4      | 5      | [      |        | ]      | 6      | 7      | 8      | 9      | 0      |  -     |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      |  @     |
+ * | TAB    | Q      | W      | E      | R      | T      | HOME   |        | PG UP  | Y      | U      | I      | O      | P      |  @     |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | LCTRL  | A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | ;      | :* /C  |
+ * | LCTRL  | A      | S      | D      | F      | G      | END    | DEL    | PG DOWN| H      | J      | K      | L      | ;      | :* /C  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | LEFT   | UP     | RIGHT  | N      | M      | ,      | .      | /      | ¥_ / S |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-    JA_HAT,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_LBRC, KC_GRV,  KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_HOME, KC_BSLS, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
+    JA_HAT,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    JA_LBRC, XXXXXXX, JA_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_HOME, XXXXXXX, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JA_AT, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_END , KC_DEL,  KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(JA_CLON),  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LEFT, KC_UP,   KC_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(JA_ENUN), \
     XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT, MO(_FN),  KC_SPC,  KC_BSPC, KC_DOWN, KC_ESC,  KC_ENT,  MO(_FN), KC_RALT, KC_GRV,  XXXXXXX, XXXXXXX  \
