a0e72a57e4714cfb3f66977cb3b2967630900d3d
Tetsurou Yamaguchi <tetsuro.yamaguchi@d2c.co.jp>
2019-12-13 13:39:32 +0900

    helix tapping term change

--- keyboards/helix/rev2/config.h
+++ keyboards/helix/rev2/config.h
@@ -20,15 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define REV2_CONFIG_H
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x3060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Yushakobo
-#define PRODUCT         Helix Beta
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x3060
+#define DEVICE_VER 0x0001
+#define MANUFACTURER Yushakobo
+#define PRODUCT Helix Beta
 #define DESCRIPTION     A split keyboard for the cheap makers
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 
 /* Use I2C or Serial */
 #define USE_I2C
@@ -43,7 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Helix keyboard OLED support
 //      see ./rules.mk: OLED_ENABLE=yes or no
 #ifdef OLED_ENABLE
-  #define SSD1306OLED
+#    define SSD1306OLED
 #endif
 
 /* Select rows configuration */
@@ -52,17 +52,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* key matrix size */
 // Rows are doubled-up
-#if  HELIX_ROWS == 4
-  #define MATRIX_ROWS 8
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+#if HELIX_ROWS == 4
+#    define MATRIX_ROWS 8
+#    define MATRIX_ROW_PINS \
+        { D4, C6, D7, E6 }
 #else
-  #define MATRIX_ROWS 10
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#    define MATRIX_ROWS 10
+#    define MATRIX_ROW_PINS \
+        { D4, C6, D7, E6, B4 }
 #endif
 
 // wiring of each half
 #define MATRIX_COLS 7
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
+#define MATRIX_COL_PINS \
+    { F4, F5, F6, F7, B1, B3, B2 }
 // #define MATRIX_COL_PINS { B2, B3, B1, F7, F6, F5, F4 } //uncomment this line and comment line above if you need to reverse left-to-right key order
 
 /* define if matrix has ghost */
@@ -88,37 +91,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define RGBLIGHT_ANIMATIONS : see ./rules.mk: LED_ANIMATIONS = yes or no
 //    see ./rules.mk: LED_BACK_ENABLE or LED_UNDERGLOW_ENABLE set yes
 #ifdef RGBLED_BACK
-  #if HELIX_ROWS == 4
-    #define RGBLED_NUM 25
-  #else
-    #define RGBLED_NUM 32
-  #endif
+#    if HELIX_ROWS == 4
+#        define RGBLED_NUM 25
+#    else
+#        define RGBLED_NUM 32
+#    endif
 #else
-  #define RGBLED_NUM 6
+#    define RGBLED_NUM 6
 #endif
 
 #ifndef IOS_DEVICE_ENABLE
-  #if RGBLED_NUM <= 6
-    #define RGBLIGHT_LIMIT_VAL 255
-  #else
-    #if HELIX_ROWS == 4
-      #define RGBLIGHT_LIMIT_VAL 130
-    #else
-      #define RGBLIGHT_LIMIT_VAL 120
-    #endif
-  #endif
-  #define RGBLIGHT_VAL_STEP 17
+#    if RGBLED_NUM <= 6
+#        define RGBLIGHT_LIMIT_VAL 255
+#    else
+#        if HELIX_ROWS == 4
+#            define RGBLIGHT_LIMIT_VAL 130
+#        else
+#            define RGBLIGHT_LIMIT_VAL 120
+#        endif
+#    endif
+#    define RGBLIGHT_VAL_STEP 17
 #else
-  #if RGBLED_NUM <= 6
-    #define RGBLIGHT_LIMIT_VAL 90
-  #else
-    #if HELIX_ROWS == 4
-      #define RGBLIGHT_LIMIT_VAL 45
-    #else
-      #define RGBLIGHT_LIMIT_VAL 35
-    #endif
-  #endif
-  #define RGBLIGHT_VAL_STEP 4
+#    if RGBLED_NUM <= 6
+#        define RGBLIGHT_LIMIT_VAL 90
+#    else
+#        if HELIX_ROWS == 4
+#            define RGBLIGHT_LIMIT_VAL 45
+#        else
+#            define RGBLIGHT_LIMIT_VAL 35
+#        endif
+#    endif
+#    define RGBLIGHT_VAL_STEP 4
 #endif
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
@@ -130,11 +133,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //  330  RGB 6
 //  300  RGB 32
 //  310  OLED & RGB 32
-  #define USB_MAX_POWER_CONSUMPTION 400
+#    define USB_MAX_POWER_CONSUMPTION 400
 #else
-  // fix iPhone and iPad power adapter issue
-  // iOS device need lessthan 100
-  #define USB_MAX_POWER_CONSUMPTION 100
+// fix iPhone and iPad power adapter issue
+// iOS device need lessthan 100
+#    define USB_MAX_POWER_CONSUMPTION 100
 #endif
 
 /*
