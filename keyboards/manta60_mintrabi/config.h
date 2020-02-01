01761d073e646aeac1c12d3f4637e2c146dfcbb8
mint-rabi <mint.rabi@gmail.com>
2020-02-01 09:53:19 +0900

    暫定のmanta60を取り込み2

--- keyboards/manta60_mintrabi/config.h
+++ keyboards/manta60_mintrabi/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    kamonanban
-#define PRODUCT         manta60
+#define PRODUCT         manta60_mintrabi
 #define DESCRIPTION     A split keyboard with 27 ortholinear keys and 5-7 thumb keys
 
 /* set the handedness */
